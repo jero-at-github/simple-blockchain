8f3f12dc8448696273bd0a2772794f7d2662f35b
Jero <jlopeznavarro@gmail.com>
2018-08-26 21:23:37 +0200

    Added proper get method and blockchain intialization

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var express = require('express');
+var simpleChain = require('./simpleChain');
 
 var app = express();
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-  res.send('hello world')
+// respond with the requested block
+app.get('/block/:blockHeight', function (req, res) {
+  res.send('hello world');
 });
 
-app.listen(8000, () => console.log('Listening on port 8000!'))
+app.listen(8000, () => {
+
+    let myPrivateBC = new simpleChain.Blockchain();   
+    myPrivateBC.initChain(true);
+    console.log('Listening on port 8000!');
+} );
