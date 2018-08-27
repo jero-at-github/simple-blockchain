4f049ea78edb8a5e5b9715364a67fadcd916cc3c
Jero <jlopeznavarro@gmail.com>
2018-08-27 22:10:48 +0200

    Working on GET block

--- index.js
+++ index.js
@@ -3,14 +3,32 @@ var simpleChain = require('./simpleChain');
 
 var app = express();
 
+let myPrivateBC = null;
+
 // respond with the requested block
-app.get('/block/:blockHeight', function (req, res) {
-  res.send('hello world');
+app.get('/block/:blockHeight', async function (req, res) {
+
+    let blockHeight = parseFloat(req.params.blockHeight);
+    let block = await myPrivateBC.getBlock(blockHeight);
+    res.send(block);
 });
 
 app.listen(8000, () => {
 
-    let myPrivateBC = new simpleChain.Blockchain();   
-    myPrivateBC.initChain(true);
-    console.log('Listening on port 8000!');
+    myPrivateBC = new simpleChain.Blockchain();    // let's instance the class
+
+    (async function init() {
+        
+        await myPrivateBC.initChain(true);      // init the chain, (set the parameter to true if you want to reset the db)  
+                                                // if the chain is empty then we create automatically the genesis block                                                       
+
+        //let's add 5 new blocks                                                
+        await myPrivateBC.addBlock(null);       
+        await myPrivateBC.addBlock(null);       
+        await myPrivateBC.addBlock(null);       
+        await myPrivateBC.addBlock(null);       
+        await myPrivateBC.addBlock(null);       
+
+        await myPrivateBC.printChainData();     // let's print the whole chain
+    })();
 } );
